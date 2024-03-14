6447850f00f0432994ae2ac5b893cb22d82cf30d
Ranon Sew <81760863+ranonsew@users.noreply.github.com>
2024-03-15 02:18:02 +0800

    attempt: connecting Unity to OBS
    working attempt committed

--- api/controller_scene.go
+++ api/controller_scene.go
@@ -13,7 +13,7 @@ import (
 */
 
 // get all available scenes
-// @returns {"Scenes": [{"sceneIndex": 0, "sceneName": ""}]}
+// @returns {"scenes": [{"sceneIndex": 0, "sceneName": ""}]}
 func GetAllScenes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -25,7 +25,7 @@ func GetAllScenes(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]any{"Scenes": res.Scenes})
+	json.NewEncoder(w).Encode(map[string]any{"scenes": res.Scenes})
 }
 
 // get the current program scene
