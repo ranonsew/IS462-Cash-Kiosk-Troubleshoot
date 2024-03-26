14c8a7aa57b7e9fea5947d5cbb702ddee0387fe6
Maaruni Pandithurai <maarunipandithurai@Maarunis-MacBook-Pro-2.local>
2024-03-26 15:55:19 +0800

    Timer and winnie npc added

---
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public TMP_Text messageText;
+    public bool WinnerTriggered = false;
+    // public GameObject winnieNPC;
+    // public Animator animator;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            messageText.SetText(timeRemaining.ToString("F2"));
+
+
+            // Trigger winnie only once:
+            if (WinnerTriggered == false & timeRemaining < 10){
+                WinnerTriggered = true;
+                TriggerWinner();
+            }
+        }else{
+            Debug.Log("Time has run out!");
+        }
+    }
+
+    void TriggerWinner(){
+        Debug.Log("WinnieTriggered");
+        // // animator = GetComponent<Animator>(); 
+        // winnieNPC = GetComponent<GameObject>(); 
+        // // animator = winnieNPC.GetComponent<Animator>();
+        // winnieNPC.GetComponent<Animator>().Play("Defeated");
+        // // WinnieNPCController.showDefeated();
+    }
+}
---
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/WinnieNPCController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnieNPCController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showDefeated(){
+        animator.Play("Defeated");
+    }
+}
+
