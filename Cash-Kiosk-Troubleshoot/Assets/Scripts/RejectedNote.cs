7e314aafb0f787689eea9fc1ef64c54a6ad13041
Chan Zhe Xiang <zxchan.2021@scis.smu.edu.sg>
2024-04-03 18:17:55 +0800

    edit sceneD

---
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/RejectedNote.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RejectedNote : MonoBehaviour
+{
+    public void EnableRigidBody(SelectExitEventArgs args)
+    {
+        GameObject note = (args.interactableObject as XRBaseInteractable).gameObject;
+
+        Rigidbody rigidbody = note.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            Debug.Log("rigidbody");
+            rigidbody.isKinematic = false;
+            Debug.Log("done");
+        }
+    }
+
+}
