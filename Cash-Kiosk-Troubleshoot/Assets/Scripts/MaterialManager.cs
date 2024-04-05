d2c8ad422a07d4ed571f00db292e787e9a0a1514
Maaruni Pandithurai <maarunipandithurai@Maarunis-MacBook-Pro-2.local>
2024-04-05 13:31:49 +0800

    pointsystem with 'completion - percentage, number of errors, time factor, overall' added

--- Cash-Kiosk-Troubleshoot/Assets/Scripts/MaterialManager.cs
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/MaterialManager.cs
@@ -15,12 +15,16 @@ public class MaterialManager : MonoBehaviour
     public Rigidbody rigidbody_notes_internal_door;
     public TMPro.TMP_Text messageText;
 
+    public int totalStepsSceneC = 3;
+    public int currentStepsSceneC;
+
     void Start()
     {
         // // Set color as Random here upon clicking it will be blue as a way of fixing the lights:
         int color = Random.Range(0, 2);
         index = color;
         Debug.Log("color"+ index.ToString());
+        currentStepsSceneC = 0;
 
     }
 
@@ -39,21 +43,27 @@ public class MaterialManager : MonoBehaviour
     public void openKioskDoor(){
         Debug.Log("openKioskDoor adding torque");
         rigidbody_kiosk_front_door.AddTorque(Vector3.up * 1000);
+        currentStepsSceneC += 1;
     }
 
     public void openNotesDoor(){
         Debug.Log("openNotesDoor adding torque");
         rigidbody_notes_front_door.AddTorque(Vector3.up * 1000);
+        currentStepsSceneC += 1;
     }
 
     public void openNotesInternalDoor(){
         Debug.Log("openNotesInternalDoor adding torque");
         rigidbody_notes_internal_door.AddTorque(Vector3.up * 1000);
+        currentStepsSceneC += 1;
     }
 
     public void checkSteps(){
         messageText.SetText("No errors");
         messageText.color = Color.green;
+        PointsManager.instance.updateScoreCompletion("SceneC", (currentStepsSceneC/totalStepsSceneC));
+        Debug.Log("completionRate: " + PointsManager.instance.points[0][0]);
+        
     }
 
 
