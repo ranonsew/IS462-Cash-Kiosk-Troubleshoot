2d5bf366ff290dc59b0637b057d1c4b47c902bf3
Ranon Sew <81760863+ranonsew@users.noreply.github.com>
2024-04-06 19:03:16 +0800

    it works
    pog
    
    * drawer pulls out and pushes back
    * knob can be turned and moved by drawer
    * knob turning affects movement of note, but not too much
    
    > the back is still hollow af

--- Cash-Kiosk-Troubleshoot/Assets/Scripts/ScenarioFNoteController.cs
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/ScenarioFNoteController.cs
@@ -19,10 +19,15 @@ public class ScenarioFNoteController : MonoBehaviour
     /// </summary>
     public RejectedNote noteStuck;
 
+    // as of current machine/note positions
     // -0.2389 | inspector
     // -3.183544 | code
+    // -2.944644 | difference
+    
+
     private float notePosMin = -0.671f;
     private float notePosMax = 0.671f;
+    private float noteTrackMax;
 
     // Start is called before the first frame update
     void Start()
@@ -45,23 +50,42 @@ public class ScenarioFNoteController : MonoBehaviour
     private void UpdateNotePosition(float val)
     {
         Debug.Log($"Knob value: {val}");
-        float newX = noteStuck.transform.position.x + val;
+        float maxRange = notePosMax - notePosMin;
+        Debug.Log($"MAX RANGE: {maxRange}");
+        float newX = (maxRange * val) + notePosMin;
         Debug.Log($"Note new X pos: {newX}");
+
+        noteStuck.transform.localPosition = new Vector3(newX, noteStuck.transform.localPosition.y, noteStuck.transform.localPosition.z);
+
+        /*
+         * -0.671 --- 0
+         *  0.671 --- 1
+         * 
+         * max len: 1.342
+         * 
+         * (1.342 * val) - 0.671
+         */
 
         // emulate end of track, preventing from getting too stuck
         // min: -0.671
         // max: 0.671
 
-        if (newX >= notePosMin && newX <= notePosMax)
-        {
-            noteStuck.transform.position = new Vector3(newX, noteStuck.transform.position.y, noteStuck.transform.position.z);
-        }
+        //if (newX >= notePosMin && newX <= notePosMax)
+        //{
+        //    noteStuck.transform.position = new Vector3(newX, noteStuck.transform.position.y, noteStuck.transform.position.z);
+        //}
 
-        if (newX < notePosMin || newX > notePosMax)
-        {
-            Debug.Log("Note going out of bounds, stop!!");
-        }
+        //if (newX < notePosMin || newX > notePosMax)
+        //{
+        //    Debug.Log("Note going out of bounds, stop!!");
+        //}
 
 
+
+        /*
+         * Issue new: on value change
+         * triggering on any click or slight movement
+         * 
+         */
     }
 }
