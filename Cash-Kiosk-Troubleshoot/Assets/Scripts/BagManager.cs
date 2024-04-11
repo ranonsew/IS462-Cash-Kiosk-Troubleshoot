8b1d636ade3feb472d62d0672ddac1691ff7b057
Lau Li Qing <63573601+liqing-lau@users.noreply.github.com>
2024-04-11 14:17:21 +0800

    wip

--- Cash-Kiosk-Troubleshoot/Assets/Scripts/BagManager.cs
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/BagManager.cs
@@ -14,37 +14,7 @@ public class BagManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            // Raycast to detect if the player is touching a hole
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                // Check if the object hit is one of the holes
-                if (hit.collider.CompareTag("hole"))
-                {
-                    // Spawn the string object and make it move through all the holes
-                    SpawnString(hit.point);
-                }
-            }
-        }
-    }
-
-    public void SpawnString(Vector3 startLocation)
-    {
-        GameObject newString = Instantiate(stringPrefab, startLocation, Quaternion.identity);
-        ZiptieManager stringMovement = newString.GetComponent<ZiptieManager>();
-        if (stringMovement != null)
-        {
-            // Pass the array of hole positions to the string movement script
-            stringMovement.SetHolePositions(holePositions);
-        }
-        else
-        {
-            Debug.LogError("String prefab does not have StringMovement component!");
-            Destroy(newString); // Destroy the incorrectly instantiated object
-        }
+        
     }
 
     public void FindHolePositions()
@@ -55,5 +25,7 @@ public class BagManager : MonoBehaviour
         {
             holePositions[i] = holeObjects[i].transform;
         }
+
+        Debug.Log(holePositions.Length);
     }
 }
