8605c400b6a83539621b43351330ab2650ff9cf4
Chan Zhe Xiang <zxchan.2021@scis.smu.edu.sg>
2024-04-18 20:25:27 +0800

    edit coin collection animation into gunny bag

--- Cash-Kiosk-Troubleshoot/Assets/Scripts/GunnyBagSocketManager.cs
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/GunnyBagSocketManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -8,9 +7,10 @@ public class GunnyBagSocketManager : XRSocketInteractor
 {
     public string[] targetTag;
     public GameObject CoinPrefab;
-    public Transform coinInstantiateTransform;
-    public Transform enterBagPosition;
-    public Transform maxDropHeight;
+    public Transform coinSpawnTransform;
+    public GameObject CoinsInGunnyPrefab;
+    public Transform CoinsInGunnyTransform;
+    public Transform GunnyBagTransform;
 
     public override bool CanHover(IXRHoverInteractable interactable)
     {
@@ -43,10 +43,11 @@ public class GunnyBagSocketManager : XRSocketInteractor
 
     public void CollectCoins()
     {
-        Vector3 coinInstantiatePosition = coinInstantiateTransform.position;
+        float spawnOffset = Random.Range(-0.1f, 0.1f);
+        Vector3 coinSpawnPosition = coinSpawnTransform.position + new Vector3(0f, spawnOffset, 0f);
 
         // Start coroutine for each coin
-        StartCoroutine(SpawnCoins(coinInstantiatePosition, 29));
+        StartCoroutine(SpawnCoins(coinSpawnPosition, 29));
     }
 
     IEnumerator SpawnCoins(Vector3 startPosition, int numCoins)
@@ -54,39 +55,34 @@ public class GunnyBagSocketManager : XRSocketInteractor
         for (int i = 0; i < numCoins; i++)
         {
             // Instantiate coin prefab
-            GameObject coin = Instantiate(CoinPrefab, startPosition, Quaternion.identity);
-            coin.GetComponent<Rigidbody>().isKinematic = true;
+            GameObject spawnedCoin = Instantiate(CoinPrefab, startPosition, Quaternion.identity);
+            spawnedCoin.GetComponent<Rigidbody>().isKinematic = true;
 
-            // Move coin into bag
-            Vector3 direction = (enterBagPosition.position - coin.transform.position).normalized;
+            // Get the Animator component of the spawned coin
+            Animator animator = spawnedCoin.GetComponent<Animator>();
 
-            // Move the coin towards the bag
-            yield return StartCoroutine(MoveObject(coin, direction, maxDropHeight));
+            if (animator != null)
+            {
+                // Play the animation
+                animator.SetTrigger("Gunny");
+            }
+            else
+            {
+                Debug.LogError("Animator component not found on the coin prefab!");
+            }
+
+            // Destroy the coin after 0.1 seconds
+            Destroy(spawnedCoin, 0.4f);
 
             // Delay before spawning the next coin
             yield return new WaitForSeconds(0.01f); // Adjust as needed
         }
-    }
 
-    IEnumerator MoveObject(GameObject coin, Vector3 direction, Transform maxDropHeight)
-    {
-        // Move the coin towards the bag
-        while (Vector3.Dot(direction, enterBagPosition.position - coin.transform.position) > 0)
-        {
-            coin.transform.Translate(direction * 1f * Time.deltaTime);
-            yield return null;
-        }
+        // Instantiate coin in gunny prefab
+        GameObject spawnCoinsInGunny = Instantiate(CoinsInGunnyPrefab, CoinsInGunnyTransform.position, Quaternion.identity);
 
-        // Drop the coin straight down until a specified height
-        coin.GetComponent<Rigidbody>().isKinematic = false;
-        while (coin.transform.position.y > maxDropHeight.position.y)
-        {
-            coin.transform.Translate(Vector3.down * 1f * Time.deltaTime);
-            yield return null;
-        }
-
-        // Once the coin reaches the minimum drop height, it should pile up
-        coin.GetComponent<Rigidbody>().isKinematic = true;
+        // Set spawnCoinsInGunny as the parent of the childObject
+        spawnCoinsInGunny.transform.SetParent(GunnyBagTransform);
     }
 
 }
