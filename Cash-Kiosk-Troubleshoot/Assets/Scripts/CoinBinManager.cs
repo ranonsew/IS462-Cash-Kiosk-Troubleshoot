af054f68defb6646e676e41563abe13e6923c20e
Lau Li Qing <63573601+liqing-lau@users.noreply.github.com>
2024-04-20 14:34:26 +0800

    fixed coin bin door

--- Cash-Kiosk-Troubleshoot/Assets/Scripts/CoinBinManager.cs
+++ Cash-Kiosk-Troubleshoot/Assets/Scripts/CoinBinManager.cs
@@ -1,41 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class CoinBinManager : MonoBehaviour
 {
     public static bool unlocked = false; 
-    public static XRGrabInteractable interactable;
     public GameObject coinPrefab;
     public Transform coinSpawnLocation;
 
+    [SerializeField] Animator coinBinAnim;
 
     private void Start()
     {
-        interactable = GetComponent<XRGrabInteractable>();
-        Debug.Log(interactable.name);
-        UpdateGrabbableState();
-        GameObject coin = Instantiate(coinPrefab, coinSpawnLocation.position, Quaternion.identity);
-
-        //if (Random.value <= 0.1f) {
-        //    GameObject coin = Instantiate(coinPrefab, coinSpawnLocation.position, Quaternion.identity);
-        //}
-
+        if (Random.value <= 0.1f) {
+            GameObject coin = Instantiate(coinPrefab, coinSpawnLocation.position, Quaternion.identity);
+        }
     }
 
-    public static void UpdateGrabbableState()
+    public void closeBin()
     {
+        Debug.Log("closing");
         if (unlocked)
         {
-            interactable.interactionLayers = LayerMask.GetMask("Default"); // Set the interaction layer(s) as needed
-            Debug.Log("default");
-        }
-        else
-        {
-            interactable.interactionLayers = LayerMask.GetMask("Grab");
-            Debug.Log("grab");
-
+            coinBinAnim.SetBool("isOpen", false);
         }
     }
+
+
 }
